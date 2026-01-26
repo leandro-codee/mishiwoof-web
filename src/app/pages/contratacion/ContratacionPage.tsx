8f331a8531573e3221140810ec6cf9a00df2eaea
LeandroMarcelo <lmarcelos2019@gmail.com>
2026-01-26 19:35:00 -0300

    v2 fix build

--- src/app/pages/contratacion/ContratacionPage.tsx
+++ src/app/pages/contratacion/ContratacionPage.tsx
@@ -11,7 +11,6 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 
 export function ContratacionPage() {
   const location = useLocation();
-  const isContratacion = location.pathname === '/contratacion';
 
   return (
     <div className="min-h-screen flex flex-col">
